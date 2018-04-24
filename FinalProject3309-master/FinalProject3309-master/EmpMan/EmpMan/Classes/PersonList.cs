2f71a61ab9df2f61d896e2c4f6c9b726d702839b
tug12561 <38503144+tug12561@users.noreply.github.com>
2018-04-24 02:44:53 -0400

    Add files via upload

---
+++ FinalProject3309-master/FinalProject3309-master/EmpMan/EmpMan/Classes/PersonList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpMan.Classes
+{
+    public class PersonList
+    {
+        public List<Person> personList = new List<Person>();
+        public PersonList()
+        {
+
+        }
+
+        public void addPerson(Person p)
+        {
+
+            personList.Add(p);
+        }
+
+        public int Count()
+        {
+            int num = personList.Count();
+            return num;
+        }
+
+    }
+}
---
+++ FinalProject3309-master/FinalProject3309-master/EmpMan/EmpMan/frmEmpMan.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EmpMan.Classes;
+
+namespace EmpMan
+{
+    public partial class frmEmpMan : Form
+    {
+        Validation validate = new Validation();
+        FormController fc = new FormController();
+        public frmEmpMan()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void frmEmpMan_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnClearForm_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //validate.checkName(txtPersonName.Text);
+            //validate.checkBirthDate(txtPersonBirthDate.Text);
+            //validate.checkID(txtPersonID.Text);
+            //validate.checkMoney(txtManagerSalary.Text);
+        }
+
+        private void btnCreateClient_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
